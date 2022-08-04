6f419d01121d94b016d16540c4a9a34123b05d88
danilo-assis <danilo.assis@tekever.com>
2022-08-04 14:42:58 +0100

    add backgound color red and blue

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,12 @@ import { ExampleComponent } from 'package-test'
 import 'package-test/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" bgColor="red"/>
+  return (
+  <>
+  <ExampleComponent text="Create React Library Example 😄" bgColor="red"/>
+  <ExampleComponent text="Create React Library Example 😄" bgColor="blue"/>
+  </>
+  );
 }
 
 export default App
