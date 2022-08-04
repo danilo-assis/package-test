41e4e6bdf50028aa556965156284f04af0eb3a10
danilo-assis <danilo.assis@tekever.com>
2022-08-04 14:19:32 +0100

    Add cra project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ text, bgColor }) => {
+  return <div className={styles[bgColor]}>Example Component: {text}</div>
 }
